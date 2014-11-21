6110090193e4454f174cf2ddcb03f43fe9bfdc88
Jonah Dahlquist <jonah@nucleussystems.com>
2014-11-20 19:09:06 -0800

    OwlCarousel's removeItem functionality is very broken, changed to destroy carousel entirely on every collection update

--- src/angular-owl-carousel.js
+++ src/angular-owl-carousel.js
@@ -13,46 +13,29 @@
             link: function(scope, element, attributes, controller, $transclude) {
 
                 var $element = $(element);
-                $element.owlCarousel(JSON.parse(attributes.owlOptions));
-
+                var owlCarousel = null;
                 var propertyName = attributes.owlCarousel;
-                var owlCarousel = $element.data('owlCarousel');
+                var options = JSON.parse(attributes.owlOptions);
 
                 scope.$watchCollection(propertyName, function(newItems, oldItems) {
-                    var found;
-                    var newItem;
-                    var $oItem;
-                    var angularItem;
-                    for (var i in newItems) {
-                        newItem = newItems[i];
 
-                        if (owlCarousel.dom.$oItems.eq(i).length == 0
-                         || !angular.equals(angular.element(owlCarousel.dom.$oItems.eq(i).children()).scope().item, newItem)) {
-                            found = false;
-                            owlCarousel.dom.$oItems.each(function() {
-                                $oItem = $(this);
-                                angularItem = angular.element($oItem.children());
-                                if (angular.equals(angularItem.scope().item, newItem)) {
-                                    owlCarousel.dom.$oItems.eq(i).before($oItem.detach());
-                                    found = true;
-                                    return false;
-                                }
-                            });
-
-                            if (found == false) {
-                                $transclude(function(clone, scope) {
-
-                                    scope.item = newItems[i];
-                                    owlCarousel.addItem(clone[1]);
-
-                                });
-                            }
-                        }
+                    if (owlCarousel) {
+                        owlCarousel.destroy();
                     }
+                    $element.empty();
+                    $element.owlCarousel(options);
+                    owlCarousel = $element.data('owlCarousel');
 
-                    while (i < owlCarousel.dom.$oItems.length) {
-                        owlCarousel.removeItem(i);
-                        i++;
+                    for (var i in newItems) {
+                        $transclude(function(clone, scope) {
+
+                            scope.item = newItems[i];
+
+                            console.log(scope.item);
+                            console.log(clone[1]);
+                            owlCarousel.addItem(clone[1], -1);
+
+                        });
                     }
                 });
             }
